a04b0e2a34cd172aab92f04948696141b7e0ae83
andreyillo <amlozano055@misena.edu.co>
2021-10-08 10:01:58 -0500

    avance portafolio

---
+++ portafolio/manual_usuario/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manual De Usuario</title>
+
+    <link rel="stylesheet" href="css/estilos.css">
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+    <header id="header">
+        <div class="container__header">
+            <div class="logo">
+                <img src="images/logo-magtimus.png" alt="">
+            </div>
+            <div class="container__nav">
+                <nav id="nav">
+                    <ul>
+                        <li><a href="http://localhost/crud_pdo/portafolio/index.php" class="select">Portafolio Inicio</a></li>
+                    </ul>
+                </nav>
+                <div class="btn__menu" id="btn_menu"><i class="fas fa-bars"></i></div>
+            </div>
+        </div>
+    </header>
+<br>
+<br>
+<br>
+<br>
+
+    <center>
+<h2>Pasos Para Elaborar El Manual De Usuario.</h2>
+</center>
+        <!-- Aquí comienza el código -->
+
+        <div class="body__page">
+            
+
+            <div class="container__card">
+
+                <div class="card c1">
+                    <div class="icon">
+                    <img src="img/1.jpg">
+                    </div>
+                    <div class="info__description">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, repellendus dolor dolorem voluptate aut expedita doloribus molestias modi libero excepturi cum perferendis laboriosam praesentium provident suscipit, eius impedit quidem magnam ipsa similique necessitatibus nam delectus nesciunt magni! Temporibus autem, reprehenderit amet quidem aliquid architecto, blanditiis, neque quam asperiores eum enim.</p>
+                       
+                    </div>
+                </div>
+
+                <div class="card c2">
+                    <div class="icon">
+                    <img src="img/2.jpg">
+                    </div>
+                    <div class="info__description">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, repellendus dolor dolorem voluptate aut expedita doloribus molestias modi libero excepturi cum perferendis laboriosam praesentium provident suscipit, eius impedit quidem magnam ipsa similique necessitatibus nam delectus nesciunt magni! Temporibus autem, reprehenderit amet quidem aliquid architecto, blanditiis, neque quam asperiores eum enim.</p>
+                        
+                    </div>
+                </div>
+
+                <div class="card c3">
+                    <div class="icon">
+                    <img src="img/3.jpg">
+                    </div>
+                    <div class="info__description">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, repellendus dolor dolorem voluptate aut expedita doloribus molestias modi libero excepturi cum perferendis laboriosam praesentium provident suscipit, eius impedit quidem magnam ipsa similique necessitatibus nam delectus nesciunt magni! Temporibus autem, reprehenderit amet quidem aliquid architecto, blanditiis, neque quam asperiores eum enim.</p>
+                       
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <center>
+<h2>Pasos Para Elaborar El Manual De Usuario.</h2>
+</center>
+        <!-- siguientes pasos -->
+        <div class="body__page">
+
+<div class="container__card">
+
+    <div class="card c1">
+        <div class="icon">
+        <img src="img/4.jpg">
+        </div>
+        <div class="info__description">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, repellendus dolor dolorem voluptate aut expedita doloribus molestias modi libero excepturi cum perferendis laboriosam praesentium provident suscipit, eius impedit quidem magnam ipsa similique necessitatibus nam delectus nesciunt magni! Temporibus autem, reprehenderit amet quidem aliquid architecto, blanditiis, neque quam asperiores eum enim.</p>
+            
+        </div>
+    </div>
+
+    <div class="card c2">
+        <div class="icon">
+        <img src="img/5.jpg">
+        </div>
+        <div class="info__description">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, repellendus dolor dolorem voluptate aut expedita doloribus molestias modi libero excepturi cum perferendis laboriosam praesentium provident suscipit, eius impedit quidem magnam ipsa similique necessitatibus nam delectus nesciunt magni! Temporibus autem, reprehenderit amet quidem aliquid architecto, blanditiis, neque quam asperiores eum enim.</p>
+          
+        </div>
+    </div>
+
+    <div class="card c3">
+        <div class="icon">
+        <img src="img/6.jpg">
+        </div>
+        <div class="info__description">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, repellendus dolor dolorem voluptate aut expedita doloribus molestias modi libero excepturi cum perferendis laboriosam praesentium provident suscipit, eius impedit quidem magnam ipsa similique necessitatibus nam delectus nesciunt magni! Temporibus autem, reprehenderit amet quidem aliquid architecto, blanditiis, neque quam asperiores eum enim.</p>
+          
+        </div>
+    </div>
+</div>
+
+</div>
+        
+        <!--FIN DEL CÓDIGO-->
+
+
+    <script src="js/script.js"></script>
+</body>
+</html>
